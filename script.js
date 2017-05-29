af9adf1f4b5756fdae0aac6d4da0c18968211f9f
A. M. Douglas <me@amdouglas.com>
2017-05-29 20:44:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@
     }
 
     doc.getElementById('lbl_no_js').style.display = 'none';
-    doc.getElementById('lbl_js').insertAdjacentHTML('afterBegin','<b>Please solve the following problem:</b>');
-    doc.getElementById('lbl_js').insertAdjacentHTML('afterEnd','<p id="question">What is ' + win._rnd1 + ' + ' + win._rnd2 + '?</p><input type="text" name="071115020F2A4F838D379A82DF1C5C92" style="margin-left:7px" size="25" maxlength="25" value="" />');
+    doc.getElementById('lbl_js').insertAdjacentHTML('afterBegin', '<b>Please solve the following problem:</b>');
+    doc.getElementById('lbl_js').insertAdjacentHTML('afterEnd', '<p id="question">What is ' + win._rnd1 + ' + ' + win._rnd2 + '?</p><input type="text" name="071115020F2A4F838D379A82DF1C5C92" style="margin-left:7px" size="25" maxlength="25" value="" />');
     _rules.getElementsByTagName('form')[0].onsubmit = process_form;
   }
 
